15362415c0a54388979df4688ffcd2f554a92f09
Lisa <lisaohlisa@gmail.com>
2013-10-27 14:20:23 -0400

    Converted to jQuery to write HTML to the DOM and error handling now works.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
 //To-Do List
 var list = [{due: "2013-10-30 19:00", completed: false}, 
-    {name : "Groceries", due: "10/26/2013", completed: false}, 
+    {name : "Groceries", due: "2013-10-26 16:00", completed: true}, 
     {name : "Homework", due: "2013-10-22 23:59", completed: true}, 
-    {name : "Buy Tent", due: "2013-11-25 16:00", completed: false}, 
-    {name : "Dance Practice", due: "2013-10-26 09:00", completed: false}];
-
-   document.write("<ul class='list-group'>");
+    {name : "Buy tent", due: "2013-11-25 16:00", completed: false}, 
+    {name : "Dance practice", due: "2013-10-26 09:00", completed: false},
+    {name : "Call mom", due: "2013-11-04 12:00", completed: false}, 
+    {name : "Buy flowers", due: "2013-10-28 23:59", completed: false}, 
+    {name : "Register for next class", due: "2013-12-01 16:00", completed: false}, 
+    {name : "Sweep house", due: "2013-10-27 23:59", completed: true}];
+    
+$(document).ready(function() {
+    //create list
+    $( "<ul class='list-group'>" ).appendTo( ".panel-body" );
     for (var i=0; i < list.length; i++){
         try {
             writeList(list[i]);
@@ -13,19 +19,23 @@ var list = [{due: "2013-10-30 19:00", completed: false},
             console.log("Error:" + error);
         }
     } 
-    document.write("</ul>");
-    
+    //determine how to display tasks according to completed status and check or missing task names
     function writeList (task){
+        //check for missing task name data
         if (typeof task.name == 'undefined'){
             throw "Name is missing!";
         } 
+        //style completed tasks and remove
         if (task.completed){
-        document.write("<li class='list-group-item done'>" + task.name + " </li>");
+        $("<li class='list-group-item done'>" + task.name + " </li>").appendTo('.list-group');
+        $(".done").remove();
         }
         else {
-        document.write("<li class='list-group-item'>" + task.name + " </li>");    
+        $("<li class='list-group-item'>" + task.name + " </li>").appendTo('.list-group');
+        
         }
     }
-
+    $( "</ul>" ).appendTo( ".panel-body" );
+});
 
    
